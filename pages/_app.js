19c0121435a7b794a1298a5ff297d4b1ff0cc7d2
happy-helmi <github@earlybirdfilms.de>
2025-03-01 18:45:26 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import "../styles/global.css";  // ✅ Globales CSS hier importieren
+import "../styles/global.css";  // ✅ Globales CSS importieren
+import Layout from "../components/Layout"; // Optional: Layout einbinden
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
