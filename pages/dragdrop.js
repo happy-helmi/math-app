6e865ef6b2f2bb04052bda343eb88c0ace16f940
happy-helmi <github@earlybirdfilms.de>
2025-03-01 19:00:44 +0100

    Create dragdrop.js

---
+++ pages/dragdrop.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+export default function DragDrop() {
+  return (
+    <div>
+      <h1>Drag & Drop - Spiel</h1>
+      <p>Hier wird ein interaktives Mathe-Spiel erscheinen.</p>
+    </div>
+  );
+}
