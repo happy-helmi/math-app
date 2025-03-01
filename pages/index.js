48b61804423de8dfdb86e2d892c1d04ee40601cc
happy-helmi <github@earlybirdfilms.de>
2025-03-01 14:37:07 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <h1>Willkommen zur Mathe-App!</h1>
+            <p>Hier kannst du Mathematik spielerisch lernen.</p>
+        </div>
+    );
+}
+
+export default Home;
