0631bda172510e724e6ba3c3c4112a3fdc8ae5d1
happy-helmi <github@earlybirdfilms.de>
2025-03-01 16:23:34 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  distDir: '../httpdocs'  // Der statische Export wird direkt nach /httpdocs geschrieben
+};
 
+module.exports = nextConfig;
