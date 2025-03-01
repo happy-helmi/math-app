2e8748f12e03ce605260f8c985d127dfcff01a8c
happy-helmi <github@earlybirdfilms.de>
2025-03-01 18:59:51 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
-import Layout from '../components/Layout';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <Layout>
+    <div>
       <h1>Willkommen zur Mathe-App!</h1>
       <p>Hier kannst du Mathematik spielerisch lernen.</p>
-    </Layout>
+      <h2>Wähle ein Spiel:</h2>
+      <ul>
+        <li><Link href="/quiz">Mathe-Quiz</Link></li>
+        <li><Link href="/dragdrop">Drag & Drop</Link></li>
+        <li><Link href="/rechnen">Rechenaufgaben</Link></li>
+      </ul>
+    </div>
   );
 }
