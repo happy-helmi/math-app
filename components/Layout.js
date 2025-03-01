0bdc263ca6b45c634c4e90c1489a1891b9e6fdb1
happy-helmi <github@earlybirdfilms.de>
2025-03-01 16:24:14 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,1 +1,13 @@
+import Header from './Header';
+import Footer from './Footer';
+import '../styles/global.css';
 
+export default function Layout({ children }) {
+  return (
+    <div className="container">
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+}
